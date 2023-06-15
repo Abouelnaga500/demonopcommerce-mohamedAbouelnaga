9fde8ebb944e5cfea2e8cff52483e2f640f444ad
moham <Abouelnagamohamed500@gmail.com>
2023-06-15 15:45:59 +0300

    project graduation

--- src/test/java/org/example/stepDef/Hooks.java
+++ src/test/java/org/example/stepDef/Hooks.java
@@ -1,4 +1,4 @@
-package org.example.stepDef;
+package org.example.stepDefs;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -11,15 +11,16 @@ public class Hooks {
     public static WebDriver driver;
 
     @Before
-    public static void setUp (){
+    public static void setUP() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.navigate().to("https://www.saucedemo.com/");
+        driver.manage().timeouts().implicitlyWait((Duration.ofSeconds(10)));
+        driver.navigate().to("https://demo.nopcommerce.com/");
     }
 
+
     @After
-    public static void tearDown (){
+    public void tearDown() {
         driver.quit();
     }
 }
